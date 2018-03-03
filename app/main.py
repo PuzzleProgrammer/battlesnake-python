88933ef478097c34f88de4400621203d425f648a
Richard M <puzzleprogrammer@gmail.com>
2018-03-03 15:55:48 -0800

    gahd

--- app/main.py
+++ app/main.py
@@ -68,26 +68,26 @@ def move():
         'taunt': '{}'.format(len(directions))
     }
 
-# def goToTarget(mySnake, x, y, validDirs):
-    # xDist = mySnake.body.data[0].x - x
-    # yDist = mySnake.body.data[0].y - y
+def goToTarget(mySnake, x, y, validDirs):
+    xDist = mySnake.body.data[0].x - x
+    yDist = mySnake.body.data[0].y - y
 
-    # desiredDirs = []
+    desiredDirs = []
 
-    # if xDist > 0 and "left" in validDirs:
-        # desiredDirs += "left"
-    # elif xDist < 0 and "right" in validDirs:
-        # desiredDirs += "right"
+    if xDist > 0 and "left" in validDirs:
+        desiredDirs += "left"
+    elif xDist < 0 and "right" in validDirs:
+        desiredDirs += "right"
 
-    # if yDist > 0 and "up" in validDirs:
-        # desiredDirs += "up"
-    # elif yDist < 0 and "down" in validDirs:
-        # desiredDirs += "down"
+    if yDist > 0 and "up" in validDirs:
+        desiredDirs += "up"
+    elif yDist < 0 and "down" in validDirs:
+        desiredDirs += "down"
 
-    # if desiredDirs.count == 0:
-        # return validDirs
+    if desiredDirs.count == 0:
+        return validDirs
 
-    # return desiredDirs
+    return desiredDirs
 
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
