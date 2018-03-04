0eda55a641d005f14f703103c8095f385e51cff2
Richard M <puzzleprogrammer@gmail.com>
2018-03-03 16:35:37 -0800

    fixvfinal

--- app/main.py
+++ app/main.py
@@ -8,6 +8,28 @@ def gridPop(data):
     grid = [[0 for x in range(data['height'])] for x in range(data['width'])]
 	return grid
 
+
+def goToTarget(mySnake, x, y, validDirs):
+    xDist = mySnake['body']['data'][0]['x'] - x
+    yDist = mySnake['body']['data'][0]['y'] - y
+
+    desiredDirs = []
+
+    if xDist > 0 and "left" in validDirs:
+        desiredDirs += "left"
+    elif xDist < 0 and "right" in validDirs:
+        desiredDirs += "right"
+
+    if yDist > 0 and "up" in validDirs:
+        desiredDirs += "up"
+    elif yDist < 0 and "down" in validDirs:
+        desiredDirs += "down"
+
+    if len(desiredDirs) == 0:
+        return validDirs
+
+    return desiredDirs
+
 @bottle.route('/')
 def static():
     return "the server is running"
@@ -66,26 +88,6 @@ def move():
         'taunt': '{}'.format(headPos['x'])
     }
 
-def goToTarget(mySnake, x, y, validDirs):
-    xDist = mySnake['body']['data'][0]['x'] - x
-    yDist = mySnake['body']['data'][0]['y'] - y
-
-    desiredDirs = []
-
-    if xDist > 0 and "left" in validDirs:
-        desiredDirs += "left"
-    elif xDist < 0 and "right" in validDirs:
-        desiredDirs += "right"
-
-    if yDist > 0 and "up" in validDirs:
-        desiredDirs += "up"
-    elif yDist < 0 and "down" in validDirs:
-        desiredDirs += "down"
-
-    if len(desiredDirs) == 0:
-        return validDirs
-
-    return desiredDirs
 
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
